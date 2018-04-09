09dd5f1d3b9fa1700db4895eff785ef911a6b9b0
Lara Inskip <larahy@gmail.com>
2018-04-09 13:21:30 -0400

    basic test to trigger heroku pipeline

---
+++ test/hello.js
@@ -0,0 +1,28 @@
+process.env.NODE_ENV = 'test';
+
+
+const port = process.env.PORT || 8080;
+const HOST = `http://localhost:${port}`;
+const PATH = "/hello/lara";
+
+const chai = require("chai");
+const should = chai.should();
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+const server = require('../server');
+
+//Our parent block
+describe('hello', () => {
+  describe('basic hello', () => {
+    it('it should say Hello', (done) => {
+      chai.request(HOST)
+        .get(PATH)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.eql('hello lara');
+          done();
+        });
+    });
+  });
+
+});
